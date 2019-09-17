8285a591ed810a3bc1ca31fc423616bd6f1f80b8
i866806 <leonardo.borba@sap.com>
2019-09-17 20:55:55 -0300

    created card service

---
+++ src/app/services/card.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Card } from '../deck-building/models/Card';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+
+  constructor() { }
+
+
+  public canBeUnlimited(card: Card): boolean{
+    if(card.cardType.toLocaleLowerCase().includes("basic land"))
+      return true
+    return false
+  }
+
+  validateQuantity(quantity: string, cardItem: Card): number {
+    let canBUnlimited = this.canBeUnlimited(cardItem);
+    let qty = Number(quantity)
+    if(!canBUnlimited)
+      if(qty > 4)
+        return 4
+    return qty
+  }
+}
